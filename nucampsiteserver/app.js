9a201329cdb67a25133013b3b0dafa189d09a3ec
Frame <kjframe1223@gmail.com>
2022-08-08 17:43:29 -0500

    https & secure communication

--- nucampsiteserver/app.js
+++ nucampsiteserver/app.js
@@ -10,7 +10,6 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
-const authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
@@ -27,6 +26,16 @@ connect.then(() => console.log('Connected correctly to server'),
 );
 
 var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

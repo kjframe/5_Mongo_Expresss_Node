209d934cfc53e9655eea2f853dadfe42a376b93e
Frame <kjframe1223@gmail.com>
2022-08-13 09:24:59 -0500

    wk4

--- nucampsiteserver/models/user.js
+++ nucampsiteserver/models/user.js
@@ -3,6 +3,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    facebookId: String,
     firstname: {
         type: String,
         default: ''
@@ -18,5 +19,14 @@ const userSchema = new Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        const token = authenticate.getToken({_id: req.user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, token: token, status: 'You are successfully logged in!'});
+    }
+});
 
 module.exports = mongoose.model('User', userSchema);
